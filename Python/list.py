3c904130b274bbafda939df0add5d23ceb767674
spctr01 <rakshitkhpi@gmail.com>
2019-10-15 20:55:01 +0530

    python

---
+++ Python/list.py
@@ -0,0 +1,28 @@
+'''Consider a list (list = []). You can perform the following commands:
+
+1.insert i e: Insert  integer  e at position i 
+
+2.print: Print the list.
+3.remove e: Delete the first occurrence of integer e
+4.append e: Insert integer e at the end of the list.
+5.sort: Sort the list.
+6.pop: Pop the last element from the list.
+7.reverse: Reverse the list.
+
+Initialize your list and read in the value of n
+followed by  n lines of commands where each command will be of the 7
+types listed above. Iterate through each command in order and perform the corresponding operation on your list.
+
+#########Input Format
+
+The first line contains an integer,n, denoting the number of commands.
+Each line i of the n subsequent lines contains one of the commands described above.
+
+#########Constraints
+The elements added to the list must be integers.
+
+'''
+#--------------------------------------------------------------------------------------------------------
+
+
+
