39f1da7f91c6c15540028fb0322b6e873eabbab9
spctr01 <rakshitkhpi@gmail.com>
2020-08-03 20:06:51 +0530

    day2

---
+++ leetcode_August_challenge/week1/Design_HashSet.py
@@ -0,0 +1,60 @@
+#problem:-
+'''Design a HashSet without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+    add(value): Insert a value into the HashSet. 
+    contains(value) : Return whether the value exists in the HashSet or not.
+    remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+
+
+Example:
+
+MyHashSet hashSet = new MyHashSet();
+hashSet.add(1);         
+hashSet.add(2);         
+hashSet.contains(1);    // returns true
+hashSet.contains(3);    // returns false (not found)
+hashSet.add(2);          
+hashSet.contains(2);    // returns true
+hashSet.remove(2);          
+hashSet.contains(2);    // returns false (already removed)'''
+
+
+#solution:-
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hs = set()
+        
+
+    def add(self, key: int) -> None:
+        if key not in self.hs:
+            self.hs.add(key)
+            
+
+    def remove(self, key: int) -> None:
+        if key  in self.hs:
+            self.hs.remove(key)
+        
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        if key  in self.hs:
+            return True
+        return False
+        
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+
