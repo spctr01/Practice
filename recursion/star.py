1679a321065dca36e7e262542b965c467121222d
rakshit <rakshitkhpi@gmail.com>
2022-01-09 13:19:44 +0530

    9-jan

---
+++ recursion/star.py
@@ -0,0 +1,21 @@
+def triangle(r,c):
+    if r==0:
+        return
+    if c<r:
+        print('*',end='')
+        triangle(r,c+1)
+        
+    else:
+        print()
+        triangle(r-1,0)
+    
+triangle(4,0)
+
+'''
+output
+****
+***
+**
+*
+
+'''
