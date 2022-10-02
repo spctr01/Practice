140ba999c7a0d2d585d403136f5c1a1731a26e48
rakshit <rakshitkhpi@gmail.com>
2022-10-02 14:27:09 +0530

    Leetcode Longest Consecutive Sequence optimise

--- Hashing/Longest Consecutive Sequence.py
+++ Hashing/Longest Consecutive Sequence.py
@@ -1,3 +1,26 @@
+##############################
+# optimised
+#############################
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n - 1) not in num_set:
+                length = 1
+                while (n + length) in num_set:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+
+
+##############################
+##############################
+
+
 class Solution:
     def check_nabour(self, num):
         flag = True
